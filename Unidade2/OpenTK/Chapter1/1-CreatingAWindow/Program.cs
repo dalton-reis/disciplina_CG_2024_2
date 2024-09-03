8e157de204c3676ef96dc92a2982213bd7d1cbbd
dalton-reis <dalton.reis@gmail.com>
2024-09-03 10:43:15 -0300

    .

---
+++ Unidade2/OpenTK/Chapter1/1-CreatingAWindow/Program.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+
+namespace LearnOpenTK
+{
+    public static class Program
+    {
+        private static void Main()
+        {
+            var nativeWindowSettings = new NativeWindowSettings()
+            {
+                ClientSize = new Vector2i(800, 600),
+                Title = "LearnOpenTK - Creating a Window",
+                // This is needed to run on macos
+                Flags = ContextFlags.ForwardCompatible,
+            };
+
+            // To create a new window, create a class that extends GameWindow, then call Run() on it.
+            using (var window = new Window(GameWindowSettings.Default, nativeWindowSettings))
+            {
+                window.Run();
+            }
+
+            // And that's it! That's all it takes to create a window with OpenTK.
+        }
+    }
+}
---
+++ Unidade2/OpenTK/Chapter1/1-CreatingAWindow/Window.cs
@@ -0,0 +1,31 @@
+﻿
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using OpenTK.Windowing.Desktop;
+
+namespace LearnOpenTK
+{
+    // This is where all OpenGL code will be written.
+    // OpenToolkit allows for several functions to be overriden to extend functionality; this is how we'll be writing code.
+    public class Window : GameWindow
+    {
+        // A simple constructor to let us set properties like window size, title, FPS, etc. on the window.
+        public Window(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings)
+            : base(gameWindowSettings, nativeWindowSettings)
+        {
+        }
+
+        // This function runs on every update frame.
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            // Check if the Escape button is currently being pressed.
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                // If it is, close the window.
+                Close();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+    }
+}
